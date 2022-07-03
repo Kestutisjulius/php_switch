52834d7ecbf7385f395cb2a4b093ef7be8ebd13a
Kęstutis Skrebė <kestutis.julius@gmail.com>
2022-07-03 14:32:33 +0300

    match magick

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo '<div style="background: bisque; text-align: center; font-size: 30px"><stro
 // dezuciu dydziai s, m, l, xl, xxl
 echo '"if" checking ';
 echo '<hr>';
-$s = 'xl';
+$s = 'ms';
 if ($s == 's'){
     echo '<h3> CHECK\'ing "S", "s" = '.$s.' </h3>';
 } if ($s == 's' || $s == 'm'){
@@ -26,6 +26,27 @@ switch ($s){
         echo '<h3> CHECK\'ing "L", "s" = '.$s.' </h3>';
     case 'xl':
         echo '<h3> CHECK\'ing "XL", "s" = '.$s.' </h3>';
+
     default : echo '<h3> not CHECK "s" = '.$s.' </h3>';
 
 }
+echo '<hr>';
+echo '<h1 style="font-family: Amiri; color: brown">"MATCH"</h1>'.'  p.s. use this ...';
+echo '<hr>';
+$res = match($s){
+    's' =>  '<h3> CHECK\'ing "S", "s" = '.$s.' </h3>',
+    'm' =>  '<h3> CHECK\'ing "M", "s" = '.$s.' </h3>',
+    'l' =>  '<h3> CHECK\'ing "L", "s" = '.$s.' </h3>',
+    'xl' =>  '<h3> CHECK\'ing "XL", "s" = '.$s.' </h3>',
+    default => '<h3> n\'CHECK "s" = '.$s.' </h3>'
+
+};
+echo "<h3>$res</h3>";
+
+$age = 5;
+$adult = match(true){
+    (call_user_func(fn()=> $age < 18)) => 'kid',
+    ($age >= 18 && $age < 66 )=> 'adult',
+    default => 'senior'
+};
+print_r($adult);
